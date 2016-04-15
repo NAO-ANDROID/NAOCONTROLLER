e4c36fa896b7cdacb8b77c3f4aa27c86729a8422
wh14 <1194672376@qq.com>
2016-04-15 08:36:31 +0800

    building

--- androidontroller/gen/com/example/naocontroller/R.java
+++ androidontroller/gen/com/example/naocontroller/R.java
@@ -27,27 +27,33 @@ public final class R {
         public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000d;
+        public static final int action_settings=0x7f080012;
         public static final int connectSure=0x7f080008;
-        public static final int customController=0x7f080009;
+        public static final int customController=0x7f08000e;
         public static final int hostProp=0x7f080005;
         public static final int hostText=0x7f080004;
         public static final int idText=0x7f080006;
         public static final int ipProp=0x7f080007;
+        public static final int moveback=0x7f08000c;
+        public static final int moveleft=0x7f08000a;
+        public static final int moverigth=0x7f08000b;
+        public static final int movetoward=0x7f080009;
         public static final int multi=0x7f080002;
-        public static final int simpleMoveController=0x7f08000b;
-        public static final int simpleTalkController=0x7f08000a;
-        public static final int simpleYuYinController=0x7f08000c;
+        public static final int simpleMoveController=0x7f080010;
+        public static final int simpleTalkController=0x7f08000f;
+        public static final int simpleYuYinController=0x7f080011;
         public static final int single=0x7f080003;
         public static final int startview=0x7f080000;
+        public static final int stopButton=0x7f08000d;
         public static final int textView1=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int connect=0x7f030001;
-        public static final int controlmenu=0x7f030002;
-        public static final int simplemovecontroller=0x7f030003;
-        public static final int startview=0x7f030004;
+        public static final int controllermove=0x7f030002;
+        public static final int controlmenu=0x7f030003;
+        public static final int simplemovecontroller=0x7f030004;
+        public static final int startview=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f070000;
---
+++ androidontroller/src/com/example/naocontroller/MessagePack.java
@@ -0,0 +1,11 @@
+package com.example.naocontroller;
+
+
+/**
+ * @author WangHuan
+ *set up to package message which is prepared to send to client
+ */
+public class MessagePack {
+	
+
+}
