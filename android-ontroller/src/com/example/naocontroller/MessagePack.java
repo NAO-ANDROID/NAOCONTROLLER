339be8a83c4bfc0b32f6e986ad76e27b4e5b21d8
tqi14 <tqi14@software.nju.edu.cn>
2016-04-16 13:13:41 +0800

    TQ

--- android-ontroller/src/com/example/naocontroller/MessagePack.java
+++ android-ontroller/src/com/example/naocontroller/MessagePack.java
@@ -8,14 +8,48 @@ package com.example.naocontroller;
 public class MessagePack {
 	
 	public String moveToward(){
-		String instruct ="";
+		String instruct ="ALMotion moveTo -p 1 0 0"; 
 		return instruct;
 	}
 	
 	public String moveRight(){
-		String instruct = "";
+		String instruct = "ALMotion moveTo -p 0.5 -0.5 -1.5709";
 		return instruct;
 	}
 	
+	public String moveLeft(){
+		String instruct = "ALMotion moveTo -p 0.5 0.5 1.5709";
+		return instruct;
+	}
+	
+	public String moveBack(){
+		String instruct ="ALMotion moveTo -p -1 0 0"; 
+		return instruct;
+	}
+	
+	public String stop(){
+		String instruct ="ALMotion stopMove -p 0 0 0"; 
+		return instruct;
+	}
+	
+	public String stretchRightHand(){
+		String instruct ="ALMotion angleInterpolation -p ['RShoulderRoll'] [1,2,3,4] [1.0,2.0,3.0,4.0] True"; 
+		return instruct;
+	}
+	
+	public String putDownRightHand(){
+		String instruct ="ALMotion angleInterpolation -p ['RShoulderRoll'] [0] [1.0] True"; 
+		return instruct;
+	}
+	
+	public String stretchLeftHand(){
+		String instruct ="ALMotion angleInterpolation -p ['LShoulderRoll'] [1,2,3,4] [1.0,2.0,3.0,4.0] True"; 
+		return instruct;
+	}
+	
+	public String putDownLeftHand(){
+		String instruct ="ALMotion angleInterpolation -p ['LShoulderRoll'] [0] [1.0] True"; 
+		return instruct;
+	}
 
 }
